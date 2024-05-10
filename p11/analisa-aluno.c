ea9ade47aea30c58720dea315015176cb158963f
mstavares <hmigueltavares@gmail.com>
2024-05-10 11:56:58 +0100

    added video with padding

--- p11/analisa-aluno.c
+++ p11/analisa-aluno.c
@@ -3,6 +3,8 @@
 int main() {
   aluno_t a;
 
+  // Tamanho da estrutura é diferente devido ao padding (otimizalção)
+  // https://www.youtube.com/watch?v=iDD6qk8Amlw
   so_debug("\nA estrutura aluno_t tem %d bytes: \
     \n\t int numero: %d bytes \
     \n\t char nome [99]: %d bytes \
